b3edd51c78ccf37fdf86e7f9d1b4b0ecc9b2344d
genevieveikechukwu <ikechukwummachukwu2020@gmail.com>
2023-12-03 22:27:08 +0100

    Change Children to Content

--- components/modal/content.tsx
+++ components/modal/content.tsx
@@ -6,10 +6,10 @@ interface ContentProps{
     
 }
 
-const Content:React.FC<ContentProps> = ({children, onClose}) => {
+const Content:React.FC<ContentProps> = ({content, onClose}) => {
   return (
     <div className="fixed h-[80vh] top-[10%] mx-auto w-1/3 z-50 border border-gray-300 rounded-lg bg-white p-5 left-[30%]">
-      <div>{children}</div>
+      <div>{content}</div>
       <button onClick={onClose} className='text-red-700 p-2 border border-gray-300 rounded text-center float-right w-28 hover:bg-red-700  hover:text-white'>Close</button>
     </div>
 
